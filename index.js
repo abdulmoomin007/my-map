fb87d32e73a19dc1f88beee3f65658ea34b95360
abdulmoomin007 <abdulmoomin007@gmail.com>
2022-07-02 15:08:35 +0530

    Updated

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 // Initialize and add the map
-async function initMap() {
-  const [lat, lng] = await getCurrentLatLng();
-  pointInMap(lat, lng);
+function initMap() {
+  init();
 }
 
 window.initMap = initMap;
 
+async function init() {
+  const [lat, lng] = await getCurrentLatLng();
+  pointInMap(lat, lng);
+}
+
 function pointInMap(lat, lng) {
   const currentLocation = { lat, lng };
   // The map, centered at currentLocation
+
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
+    zoom: 13,
     center: currentLocation,
+    // mapTypeId: "satellite",
+    // mapTypeId: "hybrid",
   });
+
   // The marker, positioned at currentLocation
+  points.forEach((point) => {
+    const distance = getDistance(currentLocation, point);
+    if (distance < 0.04)
+      new google.maps.Marker({
+        position: point,
+        map: map,
+      });
+  });
   const marker = new google.maps.Marker({
     position: currentLocation,
     map: map,
@@ -56,3 +72,22 @@ async function pointToAddress(event) {
   const [lat, lng] = await getLatLngFromZipCode(zipCode);
   pointInMap(lat, lng);
 }
+
+function getDistance({ lat: x1, lng: y1 }, { lat: x2, lng: y2 }) {
+  const a = x1 - x2;
+  const b = y1 - y2;
+  return Math.sqrt(a * a + b * b);
+}
+
+const points = [
+  { lat: 8.699997, lng: 77.66804 },
+  { lat: 8.678277, lng: 77.711041 },
+  { lat: 8.694058, lng: 77.638171 },
+  { lat: 8.707209, lng: 77.691901 },
+  { lat: 8.68218, lng: 77.754901 },
+  { lat: 8.710433, lng: 77.66232 },
+  { lat: 8.691004, lng: 77.691215 },
+  { lat: 8.690071, lng: 77.653792 },
+  { lat: 8.708651, lng: 77.691472 },
+  { lat: 8.689646, lng: 77.733958 },
+];
